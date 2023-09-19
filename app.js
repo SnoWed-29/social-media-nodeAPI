f138893826eae609ed4a08aee599b8acbb91c70d
SnoWed-29 <snowed29@gmail.com>
2023-09-19 18:05:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes.js";
+
+
+const app = express();
+
+app.use("/api/user", router); 
+mongoose.connect(
+    "mongodb+srv://admin:admin123@cluster0.y6cb01b.mongodb.net/Blog?retryWrites=true&w=majority"
+    )
+    .then(()=>app.listen(5000))
+    .then(()=>
+        console.log("application is connected to mongodb and listening to PORT 5000")
+    )
+    .catch((error)=>console.log(error));
---
+++ controllers/user-controller.js
@@ -0,0 +1,15 @@
+import User from "../model/User";
+
+export  const getAllUser = async(req, res, next) => {
+    let users;
+    try{
+        users = await User.find();
+    }catch(err){
+        console.log(err);
+    }
+    if(!users){
+        return res.status(404).json({message: "No Users Found"});
+    }
+
+    return res.status(200).json({ users })
+};
